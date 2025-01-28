2cb5fa175274997739946dba26de8308c26d85f0
Hari-42 <harisaranmohanathas@hotmail.com>
2025-01-28 23:47:55 +0100

    create window

---
+++ window.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+
+public class window {
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        frame.setSize(1000,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Tetris");
+        frame.setVisible(true);
+    }
+
+
+}
