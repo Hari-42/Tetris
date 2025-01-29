587f7942048c836334fdc3ae18553d201e10452e
Hari-42 <harisaranmohanathas@hotmail.com>
2025-01-29 23:24:12 +0100

    set window color black

--- window.java
+++ window.java
@@ -1,9 +1,11 @@
 import javax.swing.JFrame;
+import java.awt.*;
 
 
 public class window {
     public static void main(String[] args){
         JFrame frame = new JFrame();
+        frame.getContentPane().setBackground(Color.BLACK);
         frame.setSize(1000,600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Tetris");
