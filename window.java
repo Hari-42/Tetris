f7023015855a476d2109dfd1d75e52e7bcf1dfca
Hari-42 <harisaranmohanathas@hotmail.com>
2025-03-21 21:13:45 +0100

    white rectangle in the middle

--- window.java
+++ window.java
@@ -1,16 +1,34 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import java.awt.*;
 
 
-public class window {
-    public static void main(String[] args){
-        JFrame frame = new JFrame();
-        frame.getContentPane().setBackground(Color.BLACK);
-        frame.setSize(1000,600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Tetris");
-        frame.setVisible(true);
+public class window extends JFrame {
+
+
+    public window() {
+        this.getContentPane().setBackground(Color.BLACK);
+        this.setSize(1280, 720);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Tetris");
+        this.setVisible(true);
     }
 
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
 
+        int rectWidth = 360;
+        int rectHeight = 600;
+
+        int x = (getWidth() - rectWidth) / 2;
+        int y = (getHeight() - rectHeight) / 2;
+
+        g.setColor(Color.WHITE);
+        g.drawRect(x, y, rectWidth, rectHeight);
+    }
+
+    public static void main(String[] args) {
+        new window();
+    }
 }
